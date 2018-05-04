fbe9b803b242c16f882935ffc3a3dd13ed3d2246
gloriaJun <pureainu@gmail.com>
2018-05-04 20:59:42 +0900

    [Modify] route

---
+++ src/pages/_lang/live/detection.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    Live
+    <el-progress
+      :text-inside="true"
+      :stroke-width="18"
+      :percentage="0"></el-progress>
+    <el-progress
+      :text-inside="true"
+      :stroke-width="18"
+      :percentage="70"></el-progress>
+    <el-progress :percentage="70"></el-progress>
+    <el-progress
+      :percentage="50"
+      status="exception"></el-progress>
+  </div>
+</template>
+
+<script>
+export default {
+}
+</script>
---
+++ src/pages/_lang/setting/index.vue
@@ -0,0 +1,52 @@
+<template>
+  <div class="block">
+    <span class="demonstration">With shortcuts</span>
+    <el-date-picker
+      v-model="value5"
+      :picker-options="pickerOptions2"
+      type="datetimerange"
+      range-separator="To"
+      start-placeholder="Start date"
+      end-placeholder="End date"
+      align="right">
+    </el-date-picker>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      pickerOptions2: {
+        shortcuts: [{
+          text: 'Last week',
+          onClick (picker) {
+            const end = new Date()
+            const start = new Date()
+            start.setTime(start.getTime() - 3600 * 1000 * 24 * 7)
+            picker.$emit('pick', [start, end])
+          }
+        }, {
+          text: 'Last month',
+          onClick (picker) {
+            const end = new Date()
+            const start = new Date()
+            start.setTime(start.getTime() - 3600 * 1000 * 24 * 30)
+            picker.$emit('pick', [start, end])
+          }
+        }, {
+          text: 'Last 3 months',
+          onClick (picker) {
+            const end = new Date()
+            const start = new Date()
+            start.setTime(start.getTime() - 3600 * 1000 * 24 * 90)
+            picker.$emit('pick', [start, end])
+          }
+        }]
+      },
+      value4: [new Date(2000, 10, 10, 10, 10), new Date(2000, 10, 11, 10, 10)],
+      value5: ''
+    }
+  }
+}
+</script>
